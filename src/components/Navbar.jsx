5a418ff859933e2dedf0a96c912fe2ff23c8e090
umar942 <crazyop1982@gmail.com>
2025-03-15 21:48:37 +0500

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,9 +22,16 @@ const Navbar = () => {
 
       <div className="flex items-center gap-4">
         {token ? (
-          <div className="flex items-center gap-2 cursor-pointer">
+          <div className="flex items-center gap-2 cursor-pointer group relative ">
             <img className="w-10 h-10 rounded-full" src={assets.profile_pic} alt="Profile" />
             <img className="w-5" src={assets.dropdown_icon} alt="Dropdown" />
+            <div className="absolute top-0 right-0 pt-14 text-base front-medium text-gray-600 z-20 hidden group-hover:block" >
+                <div className="min-48 bg-stone-100 rounded flex flex-col gap-4 p-4" >
+                <p onClick={()=>navigate('Myprofile')} className="hover:text-black cursor-pointer"  > My Profile</p>
+                <p onClick={()=>navigate('Myappointment')} className="hover:text-black cursor-pointer"  >My Appointments</p>
+                <p  onClick={()=>setToken(false)} className="hover:text-black cursor-pointer"  >Logout </p>
+                </div>
+            </div>
           </div>
         ) : (
           <button
